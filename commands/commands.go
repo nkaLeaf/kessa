e2539da64a360c1baf7c85e95d0c03140e684cc1
nkaleaf <nkaleaf@proton.me>
2026-07-25 14:52:21 +0300

    Create commands.go

---
+++ commands/commands.go
@@ -0,0 +1,17 @@
+package commands
+
+import (
+	_ "kessa/commands/dev"
+
+	_ "kessa/commands/fun"
+
+	_ "kessa/commands/economy"
+
+	_ "kessa/commands/gambling"
+
+	_ "kessa/commands/progression"
+
+	_ "kessa/commands/utility"
+
+	_ "kessa/commands/collection"
+)
